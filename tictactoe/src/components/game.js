e3afe70cb4fc1ead76b46c54a1d626d41f41ed2a
adam <adamhansen01@gmail.com>
2022-02-20 10:19:57 -0600

    implemented history traversal

--- tictactoe/src/components/game.js
+++ tictactoe/src/components/game.js
@@ -14,11 +14,13 @@ class Game extends React.Component {
                 squares: Array(9).fill(null)
             }],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
     
     handleClick(i){
-        const history = this.state.history;
+        //history slices previous history at current stepnumber to allow moving backwards
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length -1];
         const squares = current.squares.slice();
 
@@ -35,15 +37,35 @@ class Game extends React.Component {
                 squares: squares,
             }]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         //status text being calculated here and not in the board status component is sus
         const history = this.state.history;
-        const current = history[history.length -1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step,move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+              <li key={move}>
+                  <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>  
+            );
+        });
+
+        //determine status to display
         let statusText;
         if(winner)
         {
@@ -65,8 +87,7 @@ class Game extends React.Component {
                 />
             </div>
             <div className="game-info">
-                
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
         );
