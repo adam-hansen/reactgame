80d373511a4a83aad32438899b02258294a26fc2
adam <adamhansen01@gmail.com>
2022-02-20 07:34:37 -0600

    refactored square to a simple function component

--- tictactoe/src/components/square.js
+++ tictactoe/src/components/square.js
@@ -1,25 +1,11 @@
 import React from 'react';
 
-class Square extends React.Component {
-    
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  
-  
-  render(i) {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
+function Square(props) {
+  return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
-    );
-  }
+  );
 }
 
 export default Square;
