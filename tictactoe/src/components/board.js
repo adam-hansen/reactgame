b41fc44998035fe49dbe0f2b7d49818395df7907
adam <adamhansen01@gmail.com>
2022-02-20 09:37:13 -0600

    setup status

--- tictactoe/src/components/board.js
+++ tictactoe/src/components/board.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import BoardStatus from './boardstatus';
 import Square from './square';
+
+const p1symbol = 'X';
+const p2symbol = 'O'
 
 class Board extends React.Component {
   constructor (props){
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
       
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    //handle case where someone has already clicked on square
+    if(squares[i] == null)
+    {
+      squares[i] = this.state.xIsNext ? p1symbol : p2symbol;
+        this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
+    }    
   }
 
   renderSquare(i) {
@@ -25,11 +38,24 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+
+    //status text being calculated here and not in the board status component is sus
+    const winner = calculateWinner(this.state.squares);
+
+    var statusText = '';
+    if(winner)
+    {
+      statusText = `Winner: ${winner}!`;
+    } else {
+      statusText = `Next player: ${(this.state.xIsNext ? p1symbol : p2symbol)}`;  
+    }
+    
 
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="board-status">
+          <BoardStatus value= {statusText}/>
+        </div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -48,6 +74,26 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
---
+++ tictactoe/src/components/boardstatus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BoardStatus(props) {
+  return (
+    <div className="status">
+        {props.value} 
+    </div>
+  );
+}
+
+export default BoardStatus;
