7c3f8bd74251d4fdb88e33fa9296f87b2f570a0c
adam <adamhansen01@gmail.com>
2022-02-19 23:48:11 -0600

    basics of handling state and passing properties from parent to children

--- tictactoe/src/components/square.js
+++ tictactoe/src/components/square.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class Square extends React.Component {
-    render(i) {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  
+  
+  render(i) {
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
 }
 
 export default Square;
