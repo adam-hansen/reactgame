4f04f1f877539f562299e5faad90d3a1a36e2f19
adam <adamhansen01@gmail.com>
2022-03-06 00:25:01 -0600

    wired up loading state on page load

--- tictactoe/src/components/game.js
+++ tictactoe/src/components/game.js
@@ -4,6 +4,7 @@ import BoardStatus from './boardstatus';
 
 const p1symbol = 'X';
 const p2symbol = 'O'
+const gameApiUrl = "http://localhost:3001/game"
 
 class Game extends React.Component {
     constructor (props){
@@ -36,31 +37,22 @@ class Game extends React.Component {
                 }
         });
     }
+
+    componentDidMount() {
+        fetch(gameApiUrl)
+            .then(data => data.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    xIsNext: data.p1_is_next,
+                    stepNumber: data.turn,
+                    squares: data.boardstate,
+                });
+            })
+    }
     
     handleClick(i){
-        //history slices previous history at current stepnumber to allow moving backwards
-        // const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        // const current = history[history.length -1];
-        // const squares = current.squares.slice();
-
-        // //handle case where someone has already clicked on square
-        // if(calculateWinner(squares) || squares[i])
-        // {
-        //     return;
-        // }
-        
-        // //set new value into square
-        // squares[i] = this.state.xIsNext ? p1symbol : p2symbol;
-        // this.setState({
-        //     history: history.concat([{
-        //         squares: squares,
-        //     }]),
-        //     xIsNext: !this.state.xIsNext,
-        //     stepNumber: history.length,
-        // });
-
         this.gameChannel.send({grid_position: i})
-
     }
 
     jumpTo(step) {
@@ -71,21 +63,6 @@ class Game extends React.Component {
     }
 
     render() {
-        //status text being calculated here and not in the board status component is sus
-        // const history = this.state.history;
-        // const current = history[this.state.stepNumber];
-        // const winner = calculateWinner(current.squares);
-
-        // const moves = history.map((step,move) => {
-        //     const desc = move ?
-        //         'Go to move #' + move :
-        //         'Go to game start';
-        //     return (
-        //       <li key={move}>
-        //           <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        //       </li>  
-        //     );
-        // });
 
         //determine status to display
         let statusText;
@@ -97,7 +74,6 @@ class Game extends React.Component {
         }
         
         var squarestorender = this.state.squares;
-        //var squarestorender = Array(9).fill(null);
 
         return (
         <div className="game">
