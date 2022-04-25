013d45b15203606814987c023ddd9b86aca95003
Adam Hansen <adamhansen01@gmail.com>
2022-04-25 19:50:20 +0000

    added test from another machine

--- tictactoeserver/test/models/game_test.rb
+++ tictactoeserver/test/models/game_test.rb
@@ -10,8 +10,19 @@ class GameTest < ActiveSupport::TestCase
     
     game.assign_player("player1id")
 
-    #act - call get method on game and store game in variable
     assert_equal "player1id", game.player_one_id
+  end
+
+  test "assign_player sets to player two if player one already assigned" do
+    #arrange - ensure that a game exist
+    game = Game.find_by(gameid: 'testgameid')
+    assert_nil game #not ideal to have many guards in arrange portion but some makes sense when i'm learning lifecycles
+    game = Game.create(gameid: "testgameid", p1_is_next: true, boardstate: Array.new(9,0), turn: 0, player_one_id: "testplayerid")
+    
+    game.assign_player("playertwoid")
+
+    assert_equal "testplayerid", game.player_one_id
+    assert_equal "playertwoid", game.player_two_id
   end
 
 end
